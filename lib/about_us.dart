0347132a3055408f9723090430ffa1233d0d2f30
Tanzilur Rahman <rahman.tanzilur@outlook.com>
2019-05-20 02:01:55 +0530

    Modified About Us page and added team mate pictures

--- lib/about_us.dart
+++ lib/about_us.dart
@@ -6,14 +6,49 @@ List<String> teamMembers = [
   'Sajid Iqbal',
   'Mohammad Saud',
   'Salman Ahmad',
-  'Abha Agarwal'
+  'Abha Agrawal'
+];
+
+List<String> _imageLink = [
+  'jafri.jpg',
+  'tanzil.jpeg',
+  'sajid.jpg',
+  'saud.jpg',
+  'salman.jpg',
+  'abha.jpg',
 ];
 
 Widget _buildTeamList(BuildContext context, int index) {
+  double screenHeight = MediaQuery.of(context).size.height;
+  double screenWidth = MediaQuery.of(context).size.width;
+
   return Padding(
     padding: EdgeInsets.all(9),
-    child: Text(
-      teamMembers[index],
+    child: Card(
+      color: Colors.green[100],
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          vertical: screenHeight * 0.03,
+          horizontal: screenWidth * 0.05,
+        ),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/' + _imageLink[index],
+              fit: BoxFit.fill,
+              //width: screenWidth * 0.3,
+              //height: screenHeight * 0.3,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 15.0),
+              child: Text(
+                teamMembers[index],
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          ],
+        ),
+      ),
     ),
   );
 }
